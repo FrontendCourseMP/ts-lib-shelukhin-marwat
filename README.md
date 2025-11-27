e4a1caae65c9713260ec46ac4174404148c1796d
shelukhin <shel130477@gmail.com>
2025-11-27 15:35:08 +0300

    shelukhin-Types done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Документация по вашему решению
+shelukhin-marwat
+
+shemi52-Шелухин Михаил
+Morwat Mohmand - Мохманд Морват
---
+++ src/types/index.d.ts
@@ -0,0 +1,32 @@
+export type SimpleValidatorInit = HTMLFormElement
+
+export interface FieldRules {
+    required?: boolean
+    minLength?: number
+    email?: boolean
+}
+
+export interface ValidationResult {
+    valid: boolean
+    errors: Record<string, string[]>
+}
+
+export interface AddFieldFunction {
+    (fieldName: string, rules: FieldRules): void
+}
+
+export interface ValidateFunction {
+    (): ValidationResult
+}
+
+export interface SimpleValidatorInstance {
+    form: HTMLFormElement 
+    fields: Map<string, FieldRules>
+    addField: AddFieldFunction
+    validate: ValidateFunction
+}
+
+export interface SimpleValidatorConstructor {
+    new (form: HTMLFormElement): SimpleValidatorInstance 
+}
+
